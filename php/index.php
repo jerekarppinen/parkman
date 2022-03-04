4fc2cd947dce4228777a173f4ce1157f797fd73b
Jere Karppinen <jere.karppinen@circles.fi>
2022-03-04 22:50:46 +0200

    improve response, use const

--- php/index.php
+++ php/index.php
@@ -9,16 +9,16 @@ class Carage {
 
     private $conn;
 
+    private const ALLOWED_COUNTRIES = ['Finland', 'Ukraine'];
+
     public function __construct(
         string $servername,
         string $username,
         string $password,
         string $db
     ) {
-        // Create connection
         $conn = new mysqli($servername, $username, $password, $db);
 
-        // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
@@ -26,13 +26,33 @@ class Carage {
         $this->conn = $conn;
     }
 
-    public function getAllCarages(): string
+    public function getAllCarages(): array
     {
         $query = $this->conn->query('SELECT * FROM garages ORDER BY id ASC;');
 
         $results = [];
 
         while($row = $query->fetch_assoc()) {
+            $results[] = $row;
+        }
+
+        return $results;
+    }
+
+    public function getCaragesByCountry(string $country): string
+    {
+        if(!in_array($country, self::ALLOWED_COUNTRIES)) {
+            return 'Bad country! Only these available: ' . implode(', ', self::ALLOWED_COUNTRIES);
+        }
+
+        $statement = $this->conn->prepare('SELECT * FROM garages WHERE country=?');
+        $statement->bind_param('s', $country);
+        $statement->execute();
+        $result = $statement->get_result();
+
+        $results = [];
+
+        while($row = $result->fetch_assoc()) {
             $results[] = $row;
         }
 
@@ -47,9 +67,18 @@ $carage = new Carage(
     $db
 );
 
+// Todo: sanitize inputs
 switch($_GET['query']) {
     case 'all':
-        print($carage->getAllCarages());
+        print(json_encode(
+            $response = [
+                'result' => true,
+                'garages' => $carage->getAllCarages()
+            ]
+        ));
+        break;
+    case 'country':
+        print($carage->getCaragesByCountry($_GET['country']));
         break;
 }
 
